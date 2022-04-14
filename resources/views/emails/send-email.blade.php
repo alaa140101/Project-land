d17c0e7c4d304a138ddb95757497b5157ea5014b
alaa140101 <alaa140011@gamil.com>
2022-04-14 10:59:44 +0300

    send email template

--- resources/views/emails/send-email.blade.php
+++ resources/views/emails/send-email.blade.php
@@ -1,23 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-
-  <div class="container p-3">
-    <div class="row">
-      <div class="col-8">      
+<div class="container">
+  <div class="row justify-content-center mt-3">
+    <div class="card mb-2 col-md-8">
+      <div class="card-header text-center">
+        {{__('Upload Project')}}
+      </div>
+      <div class="card-body">
         <form method="POST" action="{{route('sendbulkmail.store')}}">
           @csrf
           <div class="form-group">
             <label for="title">{{__('Title')}}</label>
-            <input type="text" name="title" class="form-control" id="title" placeholder="Title">
-          </div>    
-          <div class="form-group">
+            <input type="text" id="title"  name="title" value="{{ old('title') }}" class="form-control @error('title') is-invalid @enderror" >
+            @error('title')
+              <span class="invalid-feedback">
+                <strong>{{ $message }}</strong>
+              </span>
+            @enderror
+          </div> 
+          <div class="form-group">          
             <label for="message">{{__('Message')}}</label>
             <textarea class="form-control" name="message" id="message" rows="3"></textarea>
+          </div>           
+          <div class="form-group row mt-2">
+            <div class="col-md-4">
+              <button type="submit" class="btn btn-primary mt-2">{{__('Send')}}</button>
+            </div>
           </div>
-          <button type="submit" class="btn btn-primary mt-2">{{__('Send')}}</button>
-        </form>
+        </form>     
       </div>
     </div>
   </div>
+</div>    
 @endsection
