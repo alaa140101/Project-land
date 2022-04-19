99ea20c073c720d687c15d90994e1e52bf99e046
alaa140101 <alaa140011@gamil.com>
2022-04-19 13:01:27 +0300

    show project ar en

--- resources/views/projects/show-project.blade.php
+++ resources/views/projects/show-project.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
 <div class="container">
-  <h3>{{ $project->title}}</h3>
+  <h3>{{app()->getLocale()== 'ar' ? $project->title_ar:$project->title_en}}</h3>
   <hr>
   <br>
 
-  <p>{{$project->body}}</p>
+  <p>{{app()->getLocale()== 'ar' ? $project->body_ar:$project->body_en}}</p>
 </div>
 @endsection
