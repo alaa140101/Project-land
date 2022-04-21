2fd61a11335fa8c895121eee5f9bb1b7e6ceea50
alaa140101 <alaa140011@gmail.com>
2022-04-21 08:33:00 +0300

    try

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,11 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\{
-    User,
-    Project,
-    Subscriber,
-};
+use App\Models\Subscriber;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
