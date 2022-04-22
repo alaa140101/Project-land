788994921fff8ef0767b88e92b5bc3eb76dd2517
alaa140101 <alaa140011@gmail.com>
2022-04-22 04:05:34 +0300

    composer fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,9 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Subscriber;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use App\Models\Subscriber;
 
 class DatabaseSeeder extends Seeder
 {
