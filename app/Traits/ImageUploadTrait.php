3000b72f8d603aebb584fe5edb03b9650d251ae6
alaa140101 <alaa140011@gmail.com>
2022-04-24 16:41:53 +0300

    try fix image path

--- app/Traits/ImageUploadTrait.php
+++ app/Traits/ImageUploadTrait.php
@@ -5,22 +5,27 @@ namespace App\Traits;
 trait ImageUploadTrait
 {
 
-  protected $image_path = "storage\images\projects";
+  protected $image_path = "storage/images/projects";
   protected $img_height = 600;
   protected $img_width = 600;
 
   public function uploadImage($img)
   {
-    
+     
     $img_name = $this->imageName($img);
-    \Image::make($img)->resize($this->img_width, $this->img_height)->save(public_path($this->image_path.'\\'.$img_name));
+    \Image::make($img)->resize($this->img_width, $this->img_height);
 
+    $img->move($this->image_path,$img_name);
+      
     return "images/projects/".$img_name;
   }
 
   public function imageName($image)
   {
-    return time().'-'.$image->getClientOriginalName();
+    $file_extension = $image->getClientOriginalExtension();
+    $file_name = time().'.'.$file_extension;
+
+    return $file_name;
   }
 
 }
